27842de9ab1bab0469126a3552c8e2b177e3c33a
MOSTAFA-ELAZAZY <57857234+MOSTAFA-ELAZAZY@users.noreply.github.com>
2023-01-09 21:32:41 +0200

    add Soultion at 9.1.2023

---
+++ LeetCode/Easy/Truncate_Sentence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    class Truncate_Sentence
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] vs = s.Split(' ');
+            for (int i = 0; i < k; i++)
+            {
+                if (i > 0)
+                {
+                    stringBuilder.Append(" " + vs[i]);
+                }
+                else
+                {
+                    stringBuilder.Append(vs[i]);
+                }
+
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
