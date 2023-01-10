a101ccdb718d253fecd9aa73357dad03ac5f5eb1
MOSTAFA-ELAZAZY <57857234+MOSTAFA-ELAZAZY@users.noreply.github.com>
2023-01-10 23:25:29 +0200

    10.1.2023

---
+++ LeetCode/Easy/Count_Number_of_Pairs_With_Absolute_Difference_K.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    class Count_Number_of_Pairs_With_Absolute_Difference_K
+    {
+        public int CountKDifference(int[] nums, int k)
+        {
+            int Count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] - nums[j] == k)
+                        Count++;
+                }
+            }
+            return Count;
+        }
+    }
+}
