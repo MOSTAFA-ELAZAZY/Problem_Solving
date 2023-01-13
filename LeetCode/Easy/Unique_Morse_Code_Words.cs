9d4ea7b28d6104da334ec96afc4a1f6b36729c7e
MOSTAFA-ELAZAZY <57857234+MOSTAFA-ELAZAZY@users.noreply.github.com>
2023-01-13 15:39:29 +0200

    add soultion for 13.1.2023

---
+++ LeetCode/Easy/Unique_Morse_Code_Words.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    class Unique_Morse_Code_Words
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] morseAlphabet = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+                "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+            HashSet<string> uniqueTransformation = new HashSet<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in words[i])
+                {
+                    sb.Append(morseAlphabet[item - 'a']);
+                }
+                uniqueTransformation.Add(sb.ToString());
+            }
+            return uniqueTransformation.Count();
+        }
+
+    }
+}
