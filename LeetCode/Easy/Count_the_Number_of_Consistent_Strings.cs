dd5d5d7dbceb7428981eba295db9849f823652a4
MOSTAFA-ELAZAZY <57857234+MOSTAFA-ELAZAZY@users.noreply.github.com>
2023-01-14 23:03:17 +0200

    push soultion for 14.1.2023

---
+++ LeetCode/Easy/Count_the_Number_of_Consistent_Strings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    class Count_the_Number_of_Consistent_Strings
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            int num = words.Length;
+            int total = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                foreach (var c in words[i])
+                {
+                    if (!allowed.Contains(c))
+                    {
+                        total++;
+                        break;
+                    }
+                }
+            }
+            return num - total;
+        }
+    }
+}
