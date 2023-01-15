fdc6c66d6e638b67e6e27b77210bacdb57d6deea
MOSTAFA-ELAZAZY <57857234+MOSTAFA-ELAZAZY@users.noreply.github.com>
2023-01-15 19:31:52 +0200

    push soultion 15.1.2023

---
+++ LeetCode/Easy/Maximum_Product_Difference_Between_Two_Pairs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    class Maximum_Product_Difference_Between_Two_Pairs
+    {
+        public int MaxProductDifference(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[nums.Length - 1] * nums[nums.Length - 2]) - (nums[0] * nums[1]); ;
+        }
+    }
+}
